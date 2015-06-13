1e21301c180eb8a5105e191ea5e9b96c6efcf23c
JuanmaLambre <juanmlambre@gmail.com>
2015-06-13 13:19:16 -0300

    Update main.cpp

--- Tp/PerceptronWithMultipleDimensions/src/main.cpp
+++ Tp/PerceptronWithMultipleDimensions/src/main.cpp
@@ -10,6 +10,7 @@ int main(int argc, char** argv)
 	int iteraciones = 1;
 	if (argc == 2) {
 		iteraciones = atoi(argv[1]);
+		std::cout << "Con " << iteraciones << " iteraciones\n";
 	} else {
 		std::cout << "Iteraciones por default: 1\n";
 	}
