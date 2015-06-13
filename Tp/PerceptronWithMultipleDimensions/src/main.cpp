8222b93fa0d01eb745576b5910afbc490cb0b4bb
JuanmaLambre <juanmlambre@gmail.com>
2015-06-13 13:17:09 -0300

    Update main.cpp

--- Tp/PerceptronWithMultipleDimensions/src/main.cpp
+++ Tp/PerceptronWithMultipleDimensions/src/main.cpp
@@ -4,11 +4,17 @@
 using namespace std;
 
 
-int main()
+int main(int argc, char** argv)
 {
+	int iteraciones = 1;
+	if (argc == 2) {
+		iteraciones = atoi(argv[1]);
+	} else {
+		std::cout << "Iteraciones por default: 1\n";
+	}
 	//Misc::limpiarLabeled("../labeledTrainData.tsv", "../cleanLabeled.tsv");
 	Perceptron perceptron("cleanLabeled.tsv", "OneWordsFrequency.tsv","TwoWordsFrequency.tsv");
-	perceptron.entrenar(1);
+	perceptron.entrenar(iteraciones);
 	perceptron.calificar("testData.tsv", "results_perceptron.csv");
 
 	return 0;
